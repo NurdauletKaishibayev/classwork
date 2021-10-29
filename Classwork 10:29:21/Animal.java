6aa744f9b753d25a034b88fa5af96a4fb1b88944
Nurdaulet Kaishibayev <nkaishibayev@gmail.com>
2021-10-29 11:41:05 -0400

    Finished Animal class

--- Classwork 10:29:21/Animal.java
+++ Classwork 10:29:21/Animal.java
@@ -4,14 +4,12 @@ public class Animal {
     private String name;
 
     public Animal(String noise, int age, String name){
-        //initialize the values (hint use this.varname to clarify)
+        this.noise = noise;
+        this.age = age;
+        this.name = name;
     }
 
     public void speak(){
-      /*print the following message replacing the ? with name/age/noise:
-      My name is ?.
-      I am ? years old.
-      I say ?.
-      */
+      System.out.println("My name is " + name + ".\nI am " + age + "years old.\nI say " + noise + ".");
     }
 }
